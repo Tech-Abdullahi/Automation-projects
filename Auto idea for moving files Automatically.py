dfdc21c5cdc3e7c2ef514c57add82e056fd02fca
Abdullahi ii-psart <68358501+Abdallah-isa@users.noreply.github.com>
2020-08-17 18:28:13 -0700

    Add files via upload

---
+++ Auto idea for moving files Automatically.py
@@ -0,0 +1,36 @@
+from watchdog.observer import observer
+from watchdog.events import FileSystemEventHandler
+
+import os
+import json
+import time
+
+class MyHandler(FileSystemEventHandler):
+    i = 1
+    def on_modified(self, event):
+        new_name = "new_file" + str(self.i) + ".txt"
+        for filename in os.listdir(folder_to_track):
+            file_exists = os.path.isfile(folder_destination + "/" + new_name)
+            while file_exists:
+                self.i += 1
+                new_name = "new_file_" + str(self.i) + ".txt"
+                file_exists = os.path.isfile(folder_destination + "/" + new_name)
+
+            src = folder_to_track + "/" + filename
+            new_destination = folder_destination + "/" + new_name
+            os.rename(src, new_destination)
+
+folder_to_track = "/Users/Project/Desktop/myfolder"
+folder_destination = "/Users/Project/Desktop/newfolder"
+event_handler = MyHandler()
+observer = observer()
+observer.schedule(event_handler, folder_to_track, recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(10)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
+        
